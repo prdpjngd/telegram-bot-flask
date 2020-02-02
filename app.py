f32dc8bed611e8d0d4c508c024e61e5f7c6e8a4a
pradeep <pradeepjangid.jky@gmail.com>
2020-02-02 18:59:09 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+import telegram
+from telebot.credentials import bot_token, bot_user_name,URL
+from telebot.mastermind import get_response
+
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    if update.message.text:
+        text = update.message.text.encode('utf-8').decode()
+    elif update.message.video:
+        text = update.message.caption.encode('utf-8').decode()
+    else:
+        text = "ERROR-MDF"
+
+
+    #text = get_response(text)
+    bot.sendMessage(chat_id=chat_id, text=text, reply_to_message_id=msg_id)
+
+    return 'ok'
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/')
+def index():
+    return '.'
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "821412475:AAHZXZ0NRyLSU5WlRLCqYQgLInr_NzQnXeA"
+bot_user_name = "therealhumanbot"
+URL = "https://therealhuman.herokuapp.com/"
